ee989168382dd2d1e20f2f13ab472848b09ab171
liujuncai <1769031928@qq.com>
2022-04-18 17:50:27 +0800

    init project

---
+++ src/renderer/App.tsx
@@ -0,0 +1,42 @@
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Hello = () => {
+  const navigate = useNavigate();
+  const handleGoToTestClick = () => {
+    navigate('/test');
+  };
+
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <div onClick={handleGoToTestClick} aria-hidden="true">
+        go to test page
+      </div>
+      <div className="Hello">hello electron </div>
+    </div>
+  );
+};
+
+const Test = () => {
+  return <div>hello</div>;
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
+  );
+}
